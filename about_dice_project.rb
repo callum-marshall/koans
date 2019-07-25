8170d800b3539b6e1d988cc6972cbca67cd271d3
callum-marshall <callum_marshall@hotmail.co.uk>
2019-07-25 17:38:58 +0100

    complete about_dice_project.rb

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+
+  attr_accessor :values
+
+  def initialize
+    @sides = [1,2,3,4,5,6]
+    @values = []
+  end
+
+  def roll(number_of_dice)
+    # @values.clear - doesn't work and I can't figure out why
+    @values = []
+    number_of_dice.times { @values.push(@sides.sample) }
+  end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
